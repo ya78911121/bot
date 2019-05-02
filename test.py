2717687a8aa7f4b11161e9df60ce682c9364e289
Shin <ya78911121@gmail.com>
2019-05-02 16:56:32 +0800

    測試

---
+++ test.py
@@ -0,0 +1,81 @@
+import http.server
+import json
+import asyncio
+from botbuilder.schema import (Activity, ActivityTypes)
+from botframework.connector import ConnectorClient
+from botframework.connector.auth import (MicrosoftAppCredentials,JwtTokenValidation, SimpleCredentialProvider)
+
+APP_ID = '512aa7e4-99ab-4c00-b815-f66407939eac'
+APP_PASSWORD = 'ulhRJ1)*molpXSDXE4491+%'
+
+
+
+class BotRequestHandler(http.server.BaseHTTPRequestHandler):
+    @staticmethod
+    def __create_reply_activity(request_activity, text): #創建回復活動
+        return Activity(
+            type=ActivityTypes.message,
+            channel_id=request_activity.channel_id,
+            conversation=request_activity.conversation,
+            recipient=request_activity.from_property,
+            from_property=request_activity.recipient,
+            text=text,
+            service_url=request_activity.service_url)
+    #
+    def __handle_conversation_update_activity(self, activity): #處理對話更新活動
+        self.send_response(201)
+        self.end_headers()
+        if activity.members_added[0].id != activity.recipient.id:
+            credentials = MicrosoftAppCredentials(APP_ID, APP_PASSWORD)
+            reply = BotRequestHandler.__create_reply_activity(activity, 'Hello and welcome to the echo bot!')
+            connector = ConnectorClient(credentials, base_url=reply.service_url)
+            connector.conversations.send_to_conversation(reply.conversation.id, reply)
+
+    def __handle_message_activity(self, activity): #處理消息活動
+        self.send_response(200)
+        self.end_headers()
+        credentials = MicrosoftAppCredentials(APP_ID, APP_PASSWORD)
+        connector = ConnectorClient(credentials, base_url=activity.service_url)
+        reply = BotRequestHandler.__create_reply_activity(activity, 'You said: %s' % activity.text)
+        connector.conversations.send_to_conversation(reply.conversation.id, reply)
+
+    def __handle_authentication(self, activity): #處理認證
+        credential_provider = SimpleCredentialProvider(APP_ID, APP_PASSWORD)
+        loop = asyncio.new_event_loop()
+        try:
+            loop.run_until_complete(JwtTokenValidation.authenticate_request(
+                activity, self.headers.get("Authorization"), credential_provider))
+            return True
+        except Exception as ex:
+            self.send_response(401, ex)
+            self.end_headers()
+            return False
+        finally:
+            loop.close()
+
+    def __unhandled_activity(self): #未處理的活動
+        self.send_response(404)
+        self.end_headers()
+
+    def do_POST(self):
+        body = self.rfile.read(int(self.headers['Content-Length']))
+        data = json.loads(str(body, 'utf-8'))
+        activity = Activity.deserialize(data)
+        if not self.__handle_authentication(activity):
+            return
+        if activity.type == ActivityTypes.conversation_update.value:
+            self.__handle_conversation_update_activity(activity)
+        elif activity.type == ActivityTypes.message.value:
+            self.__handle_message_activity(activity)
+        else:
+            self.__unhandled_activity()
+
+
+
+try:
+    SERVER = http.server.HTTPServer(('localhost', 9000), BotRequestHandler)
+    print('Started http server on localhost:9000')
+    SERVER.serve_forever()
+except KeyboardInterrupt:
+    print('^C received, shutting down server')
+    SERVER.socket.close()
